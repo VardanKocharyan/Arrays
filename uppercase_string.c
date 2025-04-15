859f91e5263fb2ed739900e040cd66174bf2525c
VardanKocharyan <vardanqocharyan5@gmail.com>
2025-04-15 23:07:02 +0400

    Update uppercase_string.c

--- uppercase_string.c
+++ uppercase_string.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-int main(){
-
+int main()
+{
 	int const size = 5;
-	char arr[size] = {};
-	
-	for(int i = 0 ; i < size ; i++){
+	char arr[size] = {};	
+	for(int i = 0 ; i < size ; i++)
+	{
 		printf("arr[%d] = " , i);
 		scanf(" %c" , arr + i);
 		printf("\n");
 	}
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] >= 'a' && arr[i] <= 'z')
+		{
+            		arr[i] ^= 32;
+        	}
+    	}
 
-	printf("uppercases array is \n");
-	for(int i = 0 ; i < size ; i++){
-		printf("arr[%d] = " , i);
-		if(arr[i] & 32){
-			printf(" %c" , arr[i]^32);
-		}
-		else{
-			printf(" %c" , arr[i]);
-		}
-
-		printf("\n");
+    	printf("Uppercase array is:\n");
+	for (int i = 0; i < size; i++)
+	{
+		printf("arr[%d] = %c\n", i, arr[i]);
 	}
-
 return 0;
 }
